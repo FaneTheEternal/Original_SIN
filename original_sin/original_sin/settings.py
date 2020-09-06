fa58bcae8d05f5066eb3ddb150bda73a8eeb35de
FaneTheEternal <artemyev.vova@gmail.com>
2020-09-06 22:01:05 +0300

    опять пробуем в настройки

--- original_sin/original_sin/settings.py
+++ original_sin/original_sin/settings.py
@@ -1,6 +1,6 @@
 try:
-    from .local_settingsq import LocalSettings as Settings
+    from .local_settings import LocalSettings as Settings
 except ImportError:
-    from original_sin.global_settings import GlobalSettings as Settings
+    from .global_settings import GlobalSettings as Settings
 
 globals().update(Settings().get_settings())
