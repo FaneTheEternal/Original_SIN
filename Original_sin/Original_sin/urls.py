618b497f059a08516492d9316ec9ff67a09e27c8
FaneTheEternal <artemyev.vova@gmail.com>
2020-09-06 14:14:17 +0300

    favicon

--- Original_sin/Original_sin/urls.py
+++ Original_sin/Original_sin/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+urlpatterns += [
+    re_path(r'^favicon\.ico$', favicon_view)
 ]
