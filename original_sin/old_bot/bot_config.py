f783825ecc3fd03acc592ec7fc96d579d84c7818
FaneTheEternal <artemyev.vova@gmail.com>
2020-09-06 22:33:35 +0300

    блиять

--- original_sin/old_bot/bot_config.py
+++ original_sin/old_bot/bot_config.py
@@ -1,7 +1,5 @@
-from django.conf import settings
-
-token = getattr(settings, 'CHUVSU_VK_TOKEN')
+token = '08b47b1ffff73f193b7103e2c9bf2cdbdf285258084422b05efc198688cf67e6792ad0319ca2131a6580b'
 # confirmation token issued on the VK group web-administration page in "Callback API" section
-confirmation_token = getattr(settings, 'CHUVSU_VK_CONFIRMATION_TOKEN')
+confirmation_token = '3cfe16fe'
 # secret key for cross site request forgery protection. It will be in each VK server request
-secret_key = getattr(settings, 'CHUVSU_VK_SECRET_KEY')
+secret_key = 'b9796b9aef3b7752222546475675c51a958ce7b9c'
