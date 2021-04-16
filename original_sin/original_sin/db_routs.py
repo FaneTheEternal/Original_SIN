d8c2110d9060a80e85efed314b6c3be6d744ac83
FaneTheEternal <artemyev.vova@gmail.com>
2021-04-16 14:26:42 +0300

    Новый стиль роутинга БД

--- original_sin/original_sin/db_routs.py
+++ original_sin/original_sin/db_routs.py
@@ -1,54 +1,31 @@
-class BasicRouter(object):
-    route_app_labels = set()
-
-    def __not_implement(self, f_name):
-        raise NotImplementedError(f'Should be implement {f_name}')
-
-    def db_for_read(self, model, **hints):
-        self.__not_implement(__name__)
-
-    def db_for_write(self, model, **hints):
-        self.__not_implement(__name__)
-
-    def allow_relation(self, obj1, obj2, **hints):
-        self.__not_implement(__name__)
-
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        self.__not_implement(__name__)
-
-
-class OldBotRouter(BasicRouter):
-    """
-    A router to control all database operations on models in the
-    old_bot and contenttypes applications.
-    """
-    route_app_labels = {'old_bot'}
+class BaseRouter:
+    route_app_labels = {}
+    db_name = None
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
-            return 'old_bot_db'
+            return self.db_name
         return None
 
     def db_for_write(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
-            return 'old_bot_db'
+            return self.db_name
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        if (
-            obj1._meta.app_label in self.route_app_labels and
-            obj2._meta.app_label in self.route_app_labels
-        ):
-            return True
-
         if (
             obj1._meta.app_label in self.route_app_labels or
             obj2._meta.app_label in self.route_app_labels
         ):
-            return False
+           return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
-            return db == 'old_bot_db'
+            return db == self.db_name
         return None
+
+
+class OldBotRouter(BaseRouter):
+    route_app_labels = {'old_bot'}
+    db_name = 'old_bot_db'
