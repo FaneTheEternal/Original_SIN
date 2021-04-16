39597448765f029d618056565773267c56ad4c5b
FaneTheEternal <artemyev.vova@gmail.com>
2021-04-16 15:41:56 +0300

    статичные руты

--- original_sin/original_sin/global_settings.py
+++ original_sin/original_sin/global_settings.py
@@ -56,6 +56,16 @@ class GlobalSettings(BasicSettings):
         ]
         return white_noise + base
 
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    STATIC_ROOT = os.path.join(BasicSettings.BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(BasicSettings.BASE_DIR, 'static'),
+    )
+
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
