7c2a59eb7ab6c0072f5987092f359d4f57ef699d
FaneTheEternal <artemyev.vova@gmail.com>
2021-04-17 10:51:30 +0300

    Красоту наводим 2.0

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 cd original_sin || exit 1
+
+echo 'db migrate'
 python manage.py migrate
+echo 'db migrate old_bot_db'
 python manage.py migrate --database=old_bot_db
+echo 'db migrate postgresql'
 python manage.py migrate --database=postgresql
+
+echo 'manage fix_default_admin'
 python manage.py fix_default_admin
