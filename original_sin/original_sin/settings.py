740d6cfbf8fa707418d4d20b66bab845d14102df
Владимир <artemyev.vova@gmail.com>
2021-09-16 17:49:56 +0300

    делаем инфо настроек и новая версия питона

--- original_sin/original_sin/settings.py
+++ original_sin/original_sin/settings.py
@@ -6,13 +6,13 @@ logger = logging.getLogger(__name__)
 
 try:
     from .local_settings import LocalSettings as Settings
-    logger.warning('Loaded local settings')
+    logger.info('Loaded local settings')
 except ImportError:
     if 'LOAD_DJANGO_SETTINGS' in os.environ:
         from .global_settings import EnvironmentLoadSettings as Settings
-        logger.warning('Loaded env settings')
+        logger.info('Loaded env settings')
     else:
         from .global_settings import GlobalSettings as Settings
-        logger.warning('Loaded default settings')
+        logger.info('Loaded default settings')
 
 globals().update(Settings().get_settings())
