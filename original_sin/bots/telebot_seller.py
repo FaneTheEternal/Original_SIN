bf1a42bba8d00bdb53ea0280551ef3469d1c767a
Владимир <artemyev.vova@gmail.com>
2022-03-06 18:25:14 +0300

    костылии

--- original_sin/bots/telebot_seller.py
+++ original_sin/bots/telebot_seller.py
@@ -5,17 +5,19 @@ from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
-bot = telebot.TeleBot(getattr(settings, 'TELEBOT_SELLER_TOKEN', None))
+token = getattr(settings, 'CHUVSUGUIDE_BOT_TOKEN', None)
+
+if token:
+    bot = telebot.TeleBot(getattr(settings, 'TELEBOT_SELLER_TOKEN', None))
+
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        msg = (
+            'Добро пожаловать!\n'
+            'Этот бот поможет вам разместить объявление в чате @insta1reklama\n'
+            'Помощь по использованию бота https://t.me/c/1242266411/9\n'
+        )
+        bot.reply_to(message, msg)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    msg = (
-        'Добро пожаловать!\n'
-        'Этот бот поможет вам разместить объявление в чате @insta1reklama\n'
-        'Помощь по использованию бота https://t.me/c/1242266411/9\n'
-    )
-    bot.reply_to(message, msg)
-
-
-bot.infinity_polling()
+    bot.infinity_polling()
