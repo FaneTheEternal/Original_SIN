0c05d8c7f41a90abb75dcc03582eb7910ef6650d
Владимир <artemyev.vova@gmail.com>
2022-03-06 18:18:58 +0300

    инит нового бота

---
+++ original_sin/bots/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    logger.info(f'telebot_seller startup...')
+    from . import telebot_seller
+    logger.info('Success')
+except Exception as e:
+    import traceback
+
+    logger.error(f'Cant init telebot_seller cause: {e}')
+    logger.error(traceback.format_exc())
---
+++ original_sin/bots/telebot_seller.py
@@ -0,0 +1,18 @@
+import logging
+
+import telebot
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+bot = telebot.TeleBot(getattr(settings, 'TELEBOT_SELLER_TOKEN', None))
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    msg = (
+        'Добро пожаловать!\n'
+        'Этот бот поможет вам разместить объявление в чате @insta1reklama\n'
+        'Помощь по использованию бота https://t.me/c/1242266411/9\n'
+    )
+    bot.reply_to(message, msg)
